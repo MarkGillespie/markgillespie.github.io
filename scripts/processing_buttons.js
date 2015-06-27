fda10bdc0400d919203c8bdb89e915f058b6473c
Mark Gillespie <mark.gillespie81@verizon.net>
2015-06-27 00:56:42 -0700

    fancier Processing Interface

--- scripts/processing_buttons.js
+++ scripts/processing_buttons.js
@@ -1,51 +1,62 @@
 var p;
+var selected_button = "Boids";
 
-      function initializeProcessingCanvas() {
-        loadProcessingSketch('Boids', ['Boids.pde', 'Boid.pde'])
-      }
+function initializeProcessingCanvas() {
+  loadProcessingSketch('Boids', ['Boids.pde', 'Boid.pde'])
+}
 
-      function loadProcessingSketch(name, args) {
-        if (Processing.getInstanceById("processing_canvas")) {
-          Processing.getInstanceById("processing_canvas").exit();
-        }
+function loadProcessingSketch(name, args) {
+  $("#" + selected_button + "_button").attr({
+    class : "processing_button" 
+  });
 
-        filenames = [];
+  selected_button = name;
 
-        $.each(args, function(index, value) {
-          filenames[index] = "Processing_files/" + args[index];
-        });
+  $("#" + selected_button + "_button").attr({
+    class : "selected_processing_button" 
+  });
 
-        var p = new Processing.loadSketchFromSources('processing_canvas', filenames);
-        $("#processing_title").empty();
-        $("#processing_title").append(
-          name
-        );
+  if (Processing.getInstanceById("processing_canvas")) {
+    Processing.getInstanceById("processing_canvas").exit();
+  }
 
-        $.ajax({
-          type: "GET",
-          url: "Processing_files/ProgramDescriptions.xml",
-          dataType: "xml",
-          success: function(xml) {
-            $("#processing_description").empty();
-            $("#processing_description").append(
-                $(xml).find(name).text()
-            );
-          }
-        });
+  filenames = [];
 
-        $("#processing_source").empty();
-        $("#processing_source").append(
-          "Source: "          
-        );
+  $.each(args, function(index, value) {
+    filenames[index] = "Processing_files/" + args[index];
+  });
 
-        $.each(args, function(index, value) {
-          $("#processing_source").append (
-            $("<a>").attr({
-              href : "Processing_files/" + value
-            }).append(
-              value
-            ),
-            " "
-          );
-        });
-      }
+  var p = new Processing.loadSketchFromSources('processing_canvas', filenames);
+  $("#processing_title").empty();
+  $("#processing_title").append(
+    name
+  );
+
+  $.ajax({
+    type: "GET",
+    url: "Processing_files/ProgramDescriptions.xml",
+    dataType: "xml",
+    success: function(xml) {
+      $("#processing_description").empty();
+      $("#processing_description").append(
+          $(xml).find(name).text()
+      );
+    }
+  });
+
+  $("#processing_source").empty();
+  $("#processing_source").append(
+    "Source: "          
+  );
+
+  $.each(args, function(index, value) {
+    $("#processing_source").append (
+      $("<a>").attr({
+        href : "Processing_files/" + value
+      }).append(
+        value
+      ),
+      " "
+    );
+  });
+}
