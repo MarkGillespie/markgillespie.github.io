d9f13dba99d57b74063f9bec3e1ca3c49ca40be5
Mark Gillespie <mark.gillespie81@verizon.net>
2015-06-26 22:29:23 -0700

    moved button script to an external file

---
+++ scripts/processing_buttons.js
@@ -0,0 +1,51 @@
+var p;
+
+      function initializeProcessingCanvas() {
+        loadProcessingSketch('Boids', ['Boids.pde', 'Boid.pde'])
+      }
+
+      function loadProcessingSketch(name, args) {
+        if (Processing.getInstanceById("processing_canvas")) {
+          Processing.getInstanceById("processing_canvas").exit();
+        }
+
+        filenames = [];
+
+        $.each(args, function(index, value) {
+          filenames[index] = "Processing_files/" + args[index];
+        });
+
+        var p = new Processing.loadSketchFromSources('processing_canvas', filenames);
+        $("#processing_title").empty();
+        $("#processing_title").append(
+          name
+        );
+
+        $.ajax({
+          type: "GET",
+          url: "Processing_files/ProgramDescriptions.xml",
+          dataType: "xml",
+          success: function(xml) {
+            $("#processing_description").empty();
+            $("#processing_description").append(
+                $(xml).find(name).text()
+            );
+          }
+        });
+
+        $("#processing_source").empty();
+        $("#processing_source").append(
+          "Source: "          
+        );
+
+        $.each(args, function(index, value) {
+          $("#processing_source").append (
+            $("<a>").attr({
+              href : "Processing_files/" + value
+            }).append(
+              value
+            ),
+            " "
+          );
+        });
+      }
