abc93bb1f630f564658e158e7926388f1f8d72a3
Mark Gillespie <mark.gillespie81@verizon.net>
2015-07-01 00:25:29 -0700

    added code viewing in page

--- scripts/processing_buttons.js
+++ scripts/processing_buttons.js
@@ -2,7 +2,17 @@ var p;
 var selected_button = "Boids";
 
 function initializeProcessingCanvas() {
-  loadProcessingSketch('Boids', ['Boids.pde', 'Boid.pde'])
+  loadProcessingSketch('Boids', ['Boids.pde', 'Boid.pde']);
+}
+
+function escapeForHTML(s) {
+  s = s.replace(/</g, "&lt;");
+  s = s.replace(/>/g, "&gt;");
+  return s;
+}
+
+function stripEnding(s) {
+  return s.substring(0, s.length-4);
 }
 
 function loadProcessingSketch(name, args) {
@@ -46,19 +56,54 @@ function loadProcessingSketch(name, args) {
     }
   });
 
-  $("#processing_source").empty();
-  $("#processing_source").append(
-    "Source: "          
-  );
+  $("#processing_source_file_list").empty();
 
   $.each(args, function(index, value) {
-    $("#processing_source").append (
-      $("<a>").attr({
-        href : "Processing_files/" + value
+    $("#processing_source_file_list").append (
+      $("<span>").attr({
+        onclick: "loadSource('" + args[index] + "');",
+        class: "processing_source_file_option",
+        id: stripEnding(value)
       }).append(
         value
       ),
       " "
     );
+  });  
+
+  loadSource(args[0]);
+}
+
+function loadSource(s) {
+  $.ajax({
+    url : "Processing_files/" + s,
+    dataType: "text",
+    success : function (data) {
+      document.getElementById("processing_source").className="";
+      $("#processing_source").html(escapeForHTML(data));
+      Rainbow.color(document.getElementById("processing_source_wrapper"));
+      // console.log
+      $("#processing_source_file_list").children().each(function() {
+        $(this).attr({
+          class: "processing_source_file_option"
+        });
+      });
+      $("#" + stripEnding(s)).attr({
+        class: "processing_source_file_option_selected"
+      });
+    }
   });
+}
+
+function toggleSource() {
+  console.log($("#processing_source_container").css("display"));
+  if ($("#processing_source_container").css("display") == "none") {
+    $("#processing_source_container").css("display","block");
+    $("#processing_source_hider").empty()
+    $("#processing_source_hider").attr({class: "selected"}).append("Hide Source");
+  } else {
+    $("#processing_source_container").css("display","none");
+    $("#processing_source_hider").empty()
+    $("#processing_source_hider").attr({class: ""}).append("View Source");
+  }
 }
